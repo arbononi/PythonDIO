a19ab14c9bc011c49ffc5fa7d48783c2a3e0a174
André Rogério Bononi <arbononi@gmail.com>
2025-04-26 18:42:34 -0300

    del SistemaBancarioV20
    Esta versão foi excluída porque foi redefinida

--- SistemaBancarioV20/controllers/saldos_controller.py
+++
@@ -1,9 +0,0 @@
-from utils import user_functions
-from views.saldos_view import iniciar
-
-class SaldoController:
-    def __init__(self):
-        pass
-
-    def iniciar(self):
-        iniciar()
--- SistemaBancarioV20/database/infrabancodados.py
+++
@@ -1,4 +0,0 @@
-from database.bancodados_sqlite import BancoDeDados
-
-def cliente_datatable_create():
-    pass
